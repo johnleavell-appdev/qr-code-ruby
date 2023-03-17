2acfc6700713229a4d49256fa7a7dbd037ebb898
John Leavell <johnleavell@gmail.com>
2023-03-17 19:01:08 +0000

    starting qrcode exercise

--- qr.rb
+++ qr.rb
@@ -1,1 +1,10 @@
-p "Hello World"
+# p "Hello World"
+require "rqrcode"
+
+qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
